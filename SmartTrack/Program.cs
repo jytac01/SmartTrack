4203ee30823008d24f9606456c63a3b290c7d4a4
Joshua <ytacjoshua@gmail.com>
2025-06-13 18:07:54 +0800

    edit program.cs

--- SmartTrack/Program.cs
+++ SmartTrack/Program.cs
@@ -48,7 +48,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Account}/{action=Login}/{id?}")
     .WithStaticAssets();
 
 
